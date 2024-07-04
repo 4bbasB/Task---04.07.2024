a3fdcb580a636da133bc405fb8aaf2740b1a557b
4bbasB <abbasbayramli1719@gmail.com>
2024-07-04 10:00:06 +0400

    First

---
+++ Task - 04.07.2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task___04._07._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
